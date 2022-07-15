cbf4f5f62b86466cfaaef2a01cc249a31d1a57de
Manoj Thapa <mnjinvincible@gmail.com>
2022-07-15 12:36:50 +0545

    simple fix

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -6,63 +6,65 @@ function ContactForm() {
       <h4 className="text-2xl font-bold lg:text-4xl lg:font-extrabold lg:text-white text-gray-200 text-center border-b border-gray-400 pb-3 tracking-wide leading-5">
         Send your queries
       </h4>
-      <form className="mt-9 space-y-4">
-        <div className="space-y-2 flex flex-col">
-          <label
-            for="fullName"
-            className="text-sm font-medium text-gray-400 tracking-wide leading-4"
-          >
-            FullName
-          </label>
-          <input
-            type="text"
-            name="fullName"
-            value=""
-            className="rounded-md bg-gray-400"
-          />
-        </div>
-        <div className="space-y-2 flex flex-col">
-          <label
-            for="email"
-            className="text-sm font-medium text-gray-400 tracking-wide leading-4"
-          >
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            value=""
-            className="rounded-md bg-gray-400"
-          />
-        </div>
-        <div className="space-y-2 flex flex-col">
-          <label
-            for="phNumber"
-            className="text-sm font-medium text-gray-400 tracking-wide leading-4"
-          >
-            Phone number
-          </label>
-          <input
-            type="number"
-            name="phNumber"
-            value=""
-            className="rounded-md bg-gray-400"
-          />
-        </div>
-        <div className="space-y-2 flex flex-col">
-          <label
-            for="query"
-            className="text-sm font-medium text-gray-400 tracking-wide leading-4"
-          >
-            Query
-          </label>
-          <textarea
-            type="text"
-            name="query"
-            value=""
-            rows="6"
-            className="rounded-md bg-gray-400"
-          />
+      <form className="mt-9 space-y-10">
+        <div className="space-y-4">
+          <div className="space-y-2 flex flex-col">
+            <label
+              for="fullName"
+              className="text-sm font-medium text-gray-400 tracking-wide leading-4"
+            >
+              FullName
+            </label>
+            <input
+              type="text"
+              name="fullName"
+              value=""
+              className="rounded-md bg-gray-400"
+            />
+          </div>
+          <div className="space-y-2 flex flex-col">
+            <label
+              for="email"
+              className="text-sm font-medium text-gray-400 tracking-wide leading-4"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              value=""
+              className="rounded-md bg-gray-400"
+            />
+          </div>
+          <div className="space-y-2 flex flex-col">
+            <label
+              for="phNumber"
+              className="text-sm font-medium text-gray-400 tracking-wide leading-4"
+            >
+              Phone number
+            </label>
+            <input
+              type="number"
+              name="phNumber"
+              value=""
+              className="rounded-md bg-gray-400"
+            />
+          </div>
+          <div className="space-y-2 flex flex-col">
+            <label
+              for="query"
+              className="text-sm font-medium text-gray-400 tracking-wide leading-4"
+            >
+              Query
+            </label>
+            <textarea
+              type="text"
+              name="query"
+              value=""
+              rows="6"
+              className="rounded-md bg-gray-400"
+            />
+          </div>
         </div>
         <button
           type="submit"
